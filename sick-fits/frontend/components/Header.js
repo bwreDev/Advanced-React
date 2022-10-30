c970eb82bd36ace1457c99b1a83f0c4f257e6884
Colton Bonner <cbonner26118@gmail.com>
2022-10-30 15:10:29 -0600

    Global styles, inner styles, and server rendered base pages/styles.

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,7 +1,49 @@
+import Link from 'next/link';
+import styled from 'styled-components';
+import Nav from './Nav';
+
+const Logo = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  transform: skew(-7deg);
+  background: red;
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+  }
+`;
+
+const HeaderStyles = styled.header`
+  .nav-bar {
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .nav-sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid var(--black, black);
+  }
+`;
+
 export default function Header() {
   return (
-    <header>
-      <h1>My Nav Bar</h1>
-    </header>
+    <HeaderStyles>
+      <div className="nav-bar">
+        <Logo>
+          <Link href="/">Sick Fits</Link>
+        </Logo>
+      </div>
+      <div className="nav-sub-bar">
+        <p>Search</p>
+      </div>
+      <Nav />
+    </HeaderStyles>
   );
 }
